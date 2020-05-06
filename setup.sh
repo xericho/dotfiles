e139d429e5b3cb2785cdab40ca510e192aa72fef
erich-trabus <63758758+erich-trabus@users.noreply.github.com>
2020-05-06 09:12:14 -0700

    move dotfiles to home

--- setup.sh
+++ setup.sh
@@ -20,6 +20,8 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 # powerlevel10k theme
 git clone https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k --depth=1
+cp dotfiles/.zshrc ~
+cp dotfiles/.p10k.zsh ~
 # Set ZSH_THEME="powerlevel10k/powerlevel10k" in ~/.zshrc
 # plugins=(git zsh-syntax-highlighting zsh-autosuggestions sudo z)
 
@@ -31,7 +33,7 @@ git clone https://github.com/amix/vimrc.git ~/.vim_runtime --depth=1
 sh ~/.vim_runtime/install_awesome_vimrc.sh
 echo "set number" >> "$HOME/.vimrc"
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-cp env_setup/my_configs.vim ~/.vim_runtime
+cp dotfiles/my_configs.vim ~/.vim_runtime
 
 # lf file manager
 mkdir -p $HOME/.local/bin
@@ -41,5 +43,4 @@ echo 'export PATH="$PATH:/home/$USER/.local/bin"' >> "$HOME/.zshrc"
 # tmux stuff
 (cd; git clone https://github.com/gpakosz/.tmux.git)
 ln -s -f ~/.tmux/.tmux.conf
-cp ~/.tmux/.tmux.conf.local ~
-
+cp dotfiles/.tmux.conf.local ~
